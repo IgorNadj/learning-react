3541dc265d7efce4294d130943e0c194f8ed9382
Igor Nadj <igor@silverstripe.com>
2017-05-19 16:03:31 +1200

    Use redux to manage state instead

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from '../lib/app';
+import App from '../lib/app';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux';
+import cloneDeep from 'lodash/cloneDeep';
 
+
+let reducer = (state, action) => {
+
+	let getNextId = () => {
+		return state.todos.reduce
+			((acc, item) => {
+				return Math.max(acc, item.id) + 1;
+			},
+			1
+		);
+	}
+
+	if (action.type === 'ADD_NEW_TODO') {
+		let newState = cloneDeep(state);
+		newState.todos.push({
+			id: getNextId(),
+			title: 'NEW'
+		});
+		return newState;
+	}
+
+	return state;
+
+}
+
+let initialState = {
+	todos: [
+		{id: 1, title: 'One'},
+		{id: 2, title: 'Two'},
+	]
+}
+
+let store = createStore(
+	reducer,
+	initialState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-	<App/>,
+	<Provider store={store}>
+		<App/>
+	</Provider>,
 	document.getElementById('app')
 );
